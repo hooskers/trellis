b9417bfdccf92a1ce92321ac8e3d567eb81e06d6
Wade Johnson <wadej93@gmail.com>
2018-01-15 17:18:38 -0800

    Style the new card form button

--- src/js/components/List.js
+++ src/js/components/List.js
@@ -21,7 +21,8 @@ const ListTitleInput = ({onRenameList, listId, defaultValue, hideInput}) => {
       hideInput();
     }}
     >
-      <input placeholder="Rename list"
+      <input
+      placeholder="Rename list"
       autoFocus={true}
       defaultValue={defaultValue}
       onFocus={e => {
@@ -31,16 +32,16 @@ const ListTitleInput = ({onRenameList, listId, defaultValue, hideInput}) => {
         e.target.value = '';
         e.target.value = val;
       }}
-      onBlur={(e) => {
-        e.preventDefault();
-        if (listNameInput.value.trim() === "") {
-          hideInput();
-          return;
-        }
+      // onBlur={(e) => {
+      //   e.preventDefault();
+      //   if (listNameInput.value.trim() === "") {
+      //     hideInput();
+      //     return;
+      //   }
 
-        onRenameList(listId, listNameInput.value.trim());
-        hideInput();
-      }}
+      //   onRenameList(listId, listNameInput.value.trim());
+      //   hideInput();
+      // }}
       ref={node => {
         listNameInput = node;
       }} />
@@ -48,28 +49,86 @@ const ListTitleInput = ({onRenameList, listId, defaultValue, hideInput}) => {
   )
 }
 
-//const List = ({id, name, cardIds, boardId, onAddCard, onDeleteList, onRenameList}) => {
+const NewCardInput = ({listId, onAddCard, toggleVisibility}) => {
+  let cardNameInput, cardDescInput;
+
+  return (
+    <form className={`new-card ${newCardFormStyle}`}
+    onSubmit={e => {
+      e.preventDefault();
+      if (!cardNameInput.value.trim()) {
+        toggleVisibility();
+        return;
+      }
+
+      onAddCard(
+        listId,
+        cardNameInput.value.trim(),
+        cardDescInput.value,
+      );
+
+      cardNameInput.value = '';
+      cardDescInput.value = '';
+
+      toggleVisibility();
+    }}>
+      <input className='new-title'
+      placeholder="New card name"
+      autoFocus={true}
+      onFocus={e => {
+        //React `autocomplete` attribute places the cursor at the beginning
+        // of the input's text. This call back places the cursor at the end.
+        let val = e.target.value;
+        e.target.value = '';
+        e.target.value = val;
+      }}
+      onBlur={(e) => {
+        e.preventDefault();
+
+        toggleVisibility();
+      }}
+      ref={node => {
+        cardNameInput = node;
+      }} />
+
+      <textarea className='new-description'
+      autoComplete="off"
+      placeholder="New card description"
+      ref={node => {
+        cardDescInput = node;
+      }} />
+
+      <button className='new-card-submit' type="submit">Add card</button>
+    </form>
+  )
+}
+
 class List extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       listTitleFocused: false,
+      newCardFormVisible: false,
     }
   }
   
   toggleTitleFocus = () => {
-    this.setState({listTitleFocused: !this.state.listTitleFocused})
+    this.setState({listTitleFocused: !this.state.listTitleFocused});
+  }
+
+  toggleNewCardForm = () => {
+    this.setState({newCardFormVisible: !this.state.newCardFormVisible});
   }
 
   render() {
-    let cardNameInput, cardDescInput;
-
     return (
       <Fragment>
         <div className={`${listTitleStyle}`}>
           {!this.state.listTitleFocused ? 
-          <span className='list-title' onClick={this.toggleTitleFocus}>{this.props.name}</span> :
+          <span className='list-title' onClick={this.toggleTitleFocus}>
+            {this.props.name}
+          </span> :
           <ListTitleInput onRenameList={this.props.onRenameList} 
           listId={this.props.id}
           defaultValue={this.props.name}
@@ -77,41 +136,12 @@ class List extends Component {
           hideInput={this.toggleTitleFocus}
           />}
           <span className={`list-delete ion-trash-a`} onClick={() => this.props.onDeleteList(boardId, id)}></span>
-        </div>
+        </div>        
 
-        {/* Only show this form when list title is clicked. Same behavior as card title/description */}
-        
-
-        {/* Only show this form when list title is clicked. Same behavior as card title/description */}
-        <form 
-        onSubmit={e => {
-          e.preventDefault();
-          if (!cardNameInput.value.trim()) {
-            return;
-          }
-
-          this.props.onAddCard(
-            id,
-            cardNameInput.value.trim(),
-            cardDescInput.value
-          );
-
-          cardNameInput.value = '';
-          cardDescInput.value = '';
-        }}>
-          <input placeholder="New card name"
-          ref={node => {
-            cardNameInput = node;
-          }} />
-
-          <textarea autoComplete="off"
-          placeholder="New card description"
-          ref={node => {
-            cardDescInput = node;
-          }} />
-
-          <button type="submit">Add card</button>
-        </form>
+        {!this.state.newCardFormVisible ?
+        <span className={`${newCardButton}`} onClick={this.toggleNewCardForm}>Add new card...</span> :
+        <NewCardInput listId={this.props.id} onAddCard={this.props.onAddCard}
+        toggleVisibility={this.toggleNewCardForm}/>}
         
         {this.props.cardIds.map(cardId =>
           <div className={`card ${cardStyle}`} key={uuidv4()}>
@@ -126,7 +156,7 @@ class List extends Component {
 }
 
 const listStyle = css`
-  background-color: whitesmoke;
+  background-color: darkgrey;
   border-radius: 5px;
   box-shadow: 0px 0px 3px 1px #0000005c;
   display: flex;
@@ -136,15 +166,42 @@ const listStyle = css`
 `;
 
 const listTitleStyle = css`
+  margin-bottom: 0.25em;
+
   .list-title {
     float: left;
   }
 
   .list-delete {
     float: right;
+    cursor: pointer;
     &:hover {
       color: red;
     }
+  }
+`;
+
+const newCardButton = css`
+  border-radius: 3px;
+  background-color: lightgrey;
+  cursor: pointer;
+  padding-left: 8px;
+  padding-top: 4px;
+  padding-bottom: 4px;
+  padding-right: 8px;
+`;
+
+const newCardFormStyle = css`
+  input.new-title {
+    width: 100%;
+  }
+
+  textarea.new-description {
+    width: 100%;
+  }
+
+  button.new-card-submit {
+    width: 100%;
   }
 `;
 
