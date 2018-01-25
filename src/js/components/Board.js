2c089110571ccf8211e3d87040cf7036819c361e
Wade Johnson <wade@gmail.com>
2018-01-25 11:44:12 -0800

    Work on new list form/button

--- src/js/components/Board.js
+++ src/js/components/Board.js
@@ -17,6 +17,7 @@ class Board extends Component {
 
     this.state = {
       showNewListInput: false,
+      newListValue: '',
     }
   }
 
@@ -32,7 +33,12 @@ class Board extends Component {
         <div id="board-header" className={`${boardHeaderStyle}`}>
             {/* <div>Board ID: {id}</div> */}
             <span id="board-title" className={`${boardTitleStyle}`}>{this.props.name}</span>
-            <span id="add-list-btn" className={`${addListBtnStyle}`} onClick={e => {
+            <span id="add-list-btn"
+            className={`${addListBtnStyle}
+              ${this.state.showNewListInput ? 'new-list-form-open' : ''}
+              ${!this.state.newListValue ? 'new-list-form-empty' : ''}
+            `} 
+            onClick={e => {
               if (input && input.value.trim()) {
                 this.props.onAddList(this.props.id, input.value.trim())
                 input.value='';
@@ -40,29 +46,30 @@ class Board extends Component {
               
               this.toggleNewListInput();
             }}>
-              {this.state.showNewListInput &&
-                <form id="new-list-form" onSubmit={e => {
-                  e.preventDefault();
-                  if (!input.value.trim()) {
-                    return;
-                  }
-      
-                  this.props.onAddList(this.props.id, input.value.trim());
-                  input.value='';
+              <form id="new-list-form"
+              className={`${this.state.showNewListInput ? 'open' : 'closed'}`}
+              onSubmit={e => {
+                e.preventDefault();
+                if (!input.value.trim()) {
+                  return;
+                }
+    
+                this.props.onAddList(this.props.id, input.value.trim());
+                input.value='';
 
-                  this.toggleNewListInput();
-                }}>
-                  <input placeholder="New list name"
-                    ref={node => {
-                      input = node;
-                  }}
-                  onClick={e => {e.stopPropagation()}} />
-                  {/* <button type="submit">Add list</button> */}
-                </form>
-              }
+                this.toggleNewListInput();
+              }}>
+                <input placeholder="New list name"
+                ref={node => {
+                  input = node;
+                }}
+                onClick={e => {e.stopPropagation()}}
+                onChange={e => this.setState({newListValue: input.value.trim()})}
+                />
+                {/* <button type="submit">Add list</button> */}
+              </form>
 
               {!this.state.showNewListInput && <span className="text">New list</span>}
-              {this.state.showNewListInput && <span className="text">Add list</span>}
               <span className="icon ion-plus-round"></span>
             </span>
         </div>
@@ -112,6 +119,7 @@ const boardHeaderStyle = css`
   padding-left: 15px;
   padding-right: 15px;
   padding-top: 7px;
+  padding-bottom: 7px;
   box-shadow: 0px 1px 7px 3px #0000001f;
 `;
 
@@ -121,18 +129,97 @@ const boardTitleStyle = css`
 `;
 
 const addListBtnStyle = css`
+  position: absolute;
+  right: 0px;
+  padding: inherit;
   margin-left: auto;
   font-size: 1.15em;
   cursor: pointer;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  padding-left: 7px;
+  @media only screen and (min-device-width: 320px) and (max-device-width: 480px) {
+    padding-right: 0px;
+  }
+
+  &.new-list-form-open {
+    width: 100%;
+  }
 
   .text {
     margin-right: 8px;
+    @media only screen and (min-device-width: 320px) and (max-device-width: 480px) {
+      display: none;
+    }
+  }
+
+  .icon {
+    transition: transform 0.5s ease;
+
+    @media only screen and (min-device-width: 320px) and (max-device-width: 480px) {
+      margin: auto 25px auto 25px;
+    }
+  }
+
+  &.new-list-form-open {
+    .icon {
+      padding: 0px;
+      transform: rotate(0deg);
+    }
+
+    &.new-list-form-empty {
+      .icon {
+        transform: rotate(135deg);
+      }
+    }
   }
 
   #new-list-form {
     float: left;
-    margin-right: 8px;
     margin-bottom: 0;
+    height: 100%
+    display: flex;
+    font-size: 1.5em;
+    padding: inherit;
+    padding-left: 0px;
+    margin-left: auto;
+    margin-right: 0px;
+    transition: visibility 0s linear 0s, width 0.5s ease;
+
+    &.closed {
+      width: 0%;
+      visibility: hidden;
+      transition-delay: 0.5s, 0s;
+    }
+
+    &.open {
+      width: 25%;
+      visibility: visible;
+    }
+
+    @media only screen and (min-device-width: 320px) and (max-device-width: 480px) {
+      &.open {
+        width: 100%;
+      }
+
+      margin-right: 0px;
+      margin-left: auto;
+    }
+
+    &.closed input {
+      border: none;
+    }
+
+    input {
+      font-size: 1em;
+      width: 100%;
+      padding-left: 7px;
+      border-right: 1px solid #0000001a;
+      border-bottom: none;
+      border-left: 1px solid #0000001a;
+      border-top: none;
+    }
   }
 `;
 
