0d1596c00c9f82fa538354167da4479889e54f55
Wade Johnson <wadej93@gmail.com>
2018-02-02 14:31:10 -0800

    Make the dev server reachable from remote devices

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,6 +6,8 @@ module.exports = merge(common, {
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist',
+    host: '0.0.0.0',
+    disableHostCheck: true,
     hot: true,
   },
   plugins: [
